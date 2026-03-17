51b5daf0075dbb05e2d9f61a35dfbf402386474b
autodiag2 <autodiag@netcourrier.com>
2026-03-17 14:25:46 +0100

    test gen commands

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="obdb",
+    version="0.1.0",
+    description="Generated OBDb commands for python-OBD",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "obd",
+    ],
+    python_requires=">=3.9",
+)
---
+++ tests/basic.py
@@ -0,0 +1,13 @@
+import obdb
+import obd
+import sys
+
+if __name__ == "__main__":
+    device_location = "/dev/ttys000"
+    if 1 < len(sys.argv):
+        device_location = sys.argv[1]
+    
+    connection = obd.OBD(portstr=device_location)
+    response = connection.query(obdb.commands.Acura_TLX.TLX_GENERATOR, force=True)
+
+    print(response.value)
