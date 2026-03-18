a1cd83f9792524485ab90872c451ff912b217518
autodiag2 <autodiag@netcourrier.com>
2026-03-18 21:41:07 +0100

    v0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="obdb",
-    version="0.1.2",
+    version="0.1.3",
     description="Generated OBDb commands for python-OBD",
     packages=find_packages(),
     include_package_data=True,
